56ac41f09a9e1e5024696336af4c241c857d62d0
luizmatheus1254 <luiz.goncalves.silva25@escola.pr.gov.br>
2024-10-17 11:57:42 -0300

    mudei o nome do filme

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+// ação , comedia , terror, guerra
+
+// o asilo , 14 , comedia 
+// corações de ferro , 16 , guerrra 
+// braven, 16, ação
+// cães de ferro , 16, guerra
+// morto não fala , 14, terror
+
+let capoIdade;
+
 function setup() {
   createCanvas(400, 400);
+  campoIdade = createInpute ("5");
 }
 
 function draw() {
   background(220);
+  let idade = campoIdade.value();
+  let recomendacao = "corações de ferro";
+ text(recomendacao, width / 2, height / 2);
 }
+
+function geraRecomdacao(idade) {
+  if(idade >= 16) {
+  if (idade>=14)
+   return "Coração de ferro";
+  } else {
+   return "Morto não fala ";
+   }
+  
+    
+    
+    
+    
+}  
